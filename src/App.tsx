4c46441cd532d5004e8f16138626edeed40e2971
Rudersonvf <rudersonvf@gmail.com>
2023-05-10 10:56:41 -0300

    :tada: First commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+import VisitUs from './routes/VisitUs'
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <VisitUs />
+  )
+}
---
+++ src/routes/VisitUs/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+export default function VisitUs() {
+    return (
+        <main>
+            <section>
+                <h1>DSCarrosTop</h1>
+            </section>
+        </main>
+    );
+}

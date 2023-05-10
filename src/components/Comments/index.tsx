cd88c70cc38724d46eeadfc81442e0e03ba8e64a
Rudersonvf <rudersonvf@gmail.com>
2023-05-10 15:41:36 -0300

    feat: Component comments

---
+++ src/components/Comments/index.tsx
@@ -0,0 +1,9 @@
+import './styles.css';
+
+export default function Comments() {
+    return (
+        <div className='ct-comment-card'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae harum voluptatem ad fugit id distinctio.</p>
+        </div>
+    );
+}
--- src/routes/VisitUs/index.tsx
+++ src/routes/VisitUs/index.tsx
@@ -1,4 +1,5 @@
 import Card from '../../components/Card';
+import Comments from '../../components/Comments';
 import Header from '../../components/Header';
 import './styles.css';
 
@@ -8,10 +9,21 @@ export default function VisitUs() {
             <Header />
             <main>
                 <section className='ct-section'>
-                    <div className='ct-card-container'>
+                    <div id='ct-card-id' className='ct-card-container'>
                         <h2>Venha nos Visitar</h2>
                         <Card />
                         <Card />
+                    </div>
+                </section>
+                <section className='ct-section'>
+                    <div id='ct-comment-id' className='ct-card-container'>
+                        <h2>O que estão dizendo</h2>
+                        <Comments />
+                        <Comments />
+                        <Comments />
+                        <Comments />
+                        <Comments />
+                        <Comments />
                     </div>
                 </section>
             </main>
